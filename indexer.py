602973a3206444f255aa70a36fa058f54eeda137
Lharmona Lamouth <lharmonalamouth@dhcp-172-27-081-161.mobile.uci.edu>
2024-05-06 18:07:39 -0700

    Starting code/tokenizer code

--- indexer.py
+++ indexer.py
@@ -1,1 +1,55 @@
-print('hi')
+#Code for tokenizing when we have file path instead
+def tokenize(TextFilePath): 
+    token_list = []
+    #Opens file to be read byte by byte also catches error if file path is incorrect or wrong type
+    try:
+        with open(TextFilePath, encoding = 'utf-8') as file:
+            byte = file.read(1) #Reads 1 byte, the first in this case
+            current_token = ""
+            while byte: #Go throughs byte by byte of the file until end is reached
+                if(byte.isalnum() and byte.isascii()):
+                    #Checks if the char is valid to be in token and if not, will restart token string to be added to list
+                    current_token += byte
+                else:
+                    if len(current_token) != 0:
+                        token_list.append(current_token.lower())
+                    current_token = ""
+                byte = file.read(1)
+        if len(current_token) != 0:
+            token_list.append(current_token.lower())        
+    except FileNotFoundError:
+        print("File path incorrect or incorrect type")
+        pass
+    
+    return token_list
+
+
+#code to tokenize a document when we have it in string form
+def _tokenized(text_string):
+    token_list = []
+    current_token = ""
+    while (text_string):
+        char = text_string[0] #Reads first char
+        #Currently only breaking up tokens by if the current char is a letter used in any alphabet, not only english
+        #Easily changed by altering the if statement below
+        if char.isalpha():
+            #Checks if the char is valid to be in token and if not, will restart token string to be added to set
+            current_token += char
+        else:
+            if len(current_token) != 0:
+                token_list.append(current_token.lower())
+            current_token = ""
+        text_string[1:]
+        
+    if len(current_token) != 0:
+        token_list.append(current_token.lower())
+        
+    return token_list
+
+def _computeWordWeights(tokens_list):
+    #Initializing dictionary/map of the tokens and the number of occurances
+    word_freq = {}
+    #Iterates through all the values in the token list and updates its value in the corresponding dictionary/mapping 
+    for token in tokens_list:
+        #Checks the value of the token in the dictionary and sets it to the appropiate value either 1 or +1
+        word_freq[token] = word_freq.get(token,0)  + 1
