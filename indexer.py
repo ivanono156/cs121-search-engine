33d51ca36361ad8af560618ea39375c0190fe78b
Lharmona Lamouth <lharmonalamouth@Lharmonas-MacBook-Pro.local>
2024-05-10 17:00:59 -0700

    Implemented basic stemming and unique word count

--- indexer.py
+++ indexer.py
@@ -4,10 +4,12 @@ import sys
 from bs4 import BeautifulSoup
 from nltk import PorterStemmer
 from posting import Posting
+import re
 
-
+unique_words = set()
 # Skeleton code from lecture slides
 def build_index(documents) -> dict[str, list[Posting]]:
+    global unique_words
     # Create hashtable
     # Mapping = token: posting (document id)
     hashtable = {}
@@ -19,6 +21,7 @@ def build_index(documents) -> dict[str, list[Posting]]:
         # Add each token to the hashtable
         for token in tokens:
             # Initialize new tokens
+            unique_words.add(token)
             if token not in hashtable:
                 hashtable[token] = []
             # Map each token to its posting (which contains this document's id)
@@ -40,11 +43,11 @@ def parse(document) -> list[str]:
             # print(" Url:", json_object["url"])
             # print(" Encoding:", json_object["encoding"])
             # Parse content using bs4 (passing in json_object["content"])
-            soup = BeautifulSoup(json_object["content"], "lxml")    # Do we need to use the encoding for this step?
+            soup = BeautifulSoup(json_object["content"], "lxml-xml")    # Do we need to use the encoding for this step?
             # Find all important text (bold text and header text)
             content = soup.find_all(["strong", "b", "h1", "h2", "h3"])
             # FIXME: Also find a way to get the rest of the text besides the important text
-            tokens = _tokenized(" ".join([text.get_text() for text in content]))    # Need to stem the words before this
+            tokens = _tokenized_and_stem(" ".join([text.get_text() for text in content]))    # Need to stem the words before this
             return tokens
     except FileNotFoundError:
         print("File " + document + " not found")
@@ -78,52 +81,14 @@ def parse(document) -> list[str]:
 #         print(err)
 
 
-# Code for tokenizing when we have file path instead
-def tokenize(TextFilePath):
-    token_list = []
-    # Opens file to be read byte by byte also catches error if file path is incorrect or wrong type
-    try:
-        with open(TextFilePath, encoding = 'utf-8') as file:
-            byte = file.read(1) # Reads 1 byte, the first in this case
-            current_token = ""
-            while byte: # Go throughs byte by byte of the file until end is reached
-                if(byte.isalnum() and byte.isascii()):
-                    # Checks if the char is valid to be in token and if not, will restart token string to be added to list
-                    current_token += byte
-                else:
-                    if len(current_token) != 0:
-                        token_list.append(current_token.lower())
-                    current_token = ""
-                byte = file.read(1)
-        if len(current_token) != 0:
-            token_list.append(current_token.lower())        
-    except FileNotFoundError:
-        print("File path incorrect or incorrect type")
-        pass
-    
-    return token_list
-
-
 # Code to tokenize a document when we have it in string form
-def _tokenized(text_string):
-    token_list = []
-    current_token = ""
-    for char in text_string:
-        # Currently only breaking up tokens by if the current char is a letter used in any alphabet, not only english
-        # Easily changed by altering the if statement below
-        if char.isalpha():
-            # Checks if the char is valid to be in token and if not, will restart token string to be added to set
-            current_token += char
-        else:
-            if len(current_token) != 0:
-                token_list.append(current_token.lower())
-            current_token = ""
-        
-    if len(current_token) != 0:
-        token_list.append(current_token.lower())
-        
-    return token_list
-
+def _tokenized_and_stem(text_string):
+    # Apply stemming to each token
+    stemmer = PorterStemmer()
+    tokens = re.findall(r'\b\w+\b', text_string.lower())
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    return stemmed_tokens
+    
 
 def _computeWordWeights(tokens_list):
     # Initializing dictionary/map of the tokens and the number of occurrences
@@ -150,4 +115,4 @@ if __name__ == "__main__":
     # for k, v in sorted(table.items(), key=lambda item: item[0]):
     #     print(k, ":", v)
     print("The number of indexed documents:", len(files))
-    print("The number of unique words:", word_count)
+    print("The number of unique words:", len(unique_words))
